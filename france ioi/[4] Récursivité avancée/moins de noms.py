1dde891d88d3f8c7aa77b520fb810f91e1eb8b26
orkeilius <orkeilius@gmail.com>
2021-10-25 12:34:00 +0200

    create "moins de noms" from "changement de nom"

---
+++ france ioi/[4] Récursivité avancée/moins de noms.py
@@ -0,0 +1,25 @@
+import sys
+
+# http://www.france-ioi.org/algo/task.php?idChapter=530&iOrder=3
+
+# return all possible name
+def enum(depth, string):
+    """
+    recursive fonction for each caracther
+    """
+    for letter in letters:
+        if depth == output_size:
+            sys.stdout.write(string + letter + "\n")
+        else:
+            enum(depth + 1, string + letter)
+
+# get input
+letter_size = int(input())
+letters = list(input())
+output_size = int(input())
+
+# return number of possible name
+sys.stdout.write(str(letter_size ** output_size)+"\n")
+
+# return all possible name
+enum(1, "")
