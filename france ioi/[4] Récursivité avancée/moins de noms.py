22453115940eb777253df5bd38f6b65866e154b6
orkeilius <orkeilius@gmail.com>
2021-10-25 13:51:06 +0200

    update "moins de noms"

--- france ioi/[4] Récursivité avancée/moins de noms.py
+++ france ioi/[4] Récursivité avancée/moins de noms.py
@@ -1,25 +1,33 @@
+import math
 import sys
 
 # http://www.france-ioi.org/algo/task.php?idChapter=530&iOrder=3
 
 # return all possible name
-def enum(depth, string):
+def enum(depth, string, dict):
     """
     recursive fonction for each caracther
+    like "changement de nom" but with is own dictionary to remove word with 2 letter
     """
-    for letter in letters:
+    for letter in dict:
         if depth == output_size:
             sys.stdout.write(string + letter + "\n")
         else:
-            enum(depth + 1, string + letter)
+            l = dict.copy()
+            l.remove(letter)
+            enum(depth + 1, string + letter, l)
+
 
 # get input
 letter_size = int(input())
 letters = list(input())
 output_size = int(input())
 
-# return number of possible name
-sys.stdout.write(str(letter_size ** output_size)+"\n")
+# return number of possible nam
+t = 1
+for i in range(output_size):
+    t = t * (letter_size - i)
+sys.stdout.write(str(t)+"\n")
 
 # return all possible name
-enum(1, "")
+enum(1, "", letters)
