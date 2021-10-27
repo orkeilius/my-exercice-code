275ca757e0496ef65280c628c47b6cc75c53dad3
orkeilius <orkeilius@mail.com>
2021-10-27 10:50:12 +0200

    add " nombre de paquets"

--- france ioi/[4] Algorithmes semi-numeriques (1)/Nombre de paquets.py
+++ france ioi/[4] Algorithmes semi-numeriques (1)/Nombre de paquets.py
@@ -5,13 +5,12 @@ def iter(nb,depth):
     global t
     if depth != 0:
         for i in range(nb):
-            iter(i,depth + 1)
+            iter(i,depth -1)
     else:
         t += nb
-        print(nb)
 
 nbCard = int(input())
 nbRoll = int(input())
 t = 0
-iter(nbCard,nbRoll)
+iter(nbCard,nbRoll - 1)
 print(t)
