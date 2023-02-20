6680d5838bb0e4621f58dd50a7e100ac64596bf1
orkeilius <orkeilius@users.noreply.github.com>
2023-02-20 10:02:57 +0100

    add Compter les chemins vers la sortie

---
+++ france ioi/[4] Graphes/Compter les chemins vers la sortie/main.py
@@ -0,0 +1,28 @@
+import copy
+MOVES = [(-1,0),(1,0),(0,1),(0,-1)]
+
+def main(size,plane):
+    plane[1][0] = "*"
+    plane[-1][-2] = "@"
+    return dfs(size,plane,(1,1))
+
+def dfs(size,plane,pos):
+    total = 0
+    plane[pos[0]][pos[1]] = "*"
+
+    for move in MOVES:
+        nextPos = [pos[0] + move[0], pos[1] + move[1]]
+        if plane[nextPos[0]][nextPos[1]] == "@":
+            return 1 
+        if plane[nextPos[0]][nextPos[1]] == ".":
+            nextPlane = copy.deepcopy(plane)
+            total += dfs(size,nextPlane,nextPos)
+    return total
+
+
+    
+
+if __name__ == "__main__":
+    size = list(map(int,input().split()))
+    plane = [list(input()) for i in range(size[0])]
+    print(main(size,plane))
