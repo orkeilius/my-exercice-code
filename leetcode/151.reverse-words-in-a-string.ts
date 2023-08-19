3b3134a0e80152678cb75cc7d1d51674c44fb662
orkeilius <orkeilius@users.noreply.github.com>
2023-08-19 15:10:04 +0200

    add leetcode [151] Reverse Words in a String

---
+++ leetcode/151.reverse-words-in-a-string.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=151 lang=typescript
+ *
+ * [151] Reverse Words in a String
+ */
+
+// @lc code=start
+function reverseWords(s: string): string {
+    return s.split(" ").filter(e => e != "").reverse().join(" ")
+};
+// @lc code=end
